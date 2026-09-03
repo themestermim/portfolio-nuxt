5d11bea49b9c7e7ae06d1813b72387252bc6427d
mehran mahmoudi <themestermim@gmail.com>
2026-09-03 11:02:30 +0330

    init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,17 @@
-import withNuxt from './.nuxt/eslint.config.mjs'
+// import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-    {
-        rules: {
-            'no-var': 'error',
-            'prefer-const': 'error',
-            'eqeqeq': ['error', 'always'],
-            'no-console': 'warn',
-            'semi': ['error', 'never'],
-            'comma-dangle': ['error', 'always-multiline'],
-            'vue/no-mutating-props': 'error',
-            'vue/no-unused-components': 'warn',
-            'vue/multi-word-component-names': 'off',
-        },
-    },
-)
+// export default withNuxt(
+//     {
+//         rules: {
+//             'no-var': 'error',
+//             'prefer-const': 'error',
+//             'eqeqeq': ['error', 'always'],
+//             'no-console': 'warn',
+//             'semi': ['error', 'never'],
+//             'comma-dangle': ['error', 'always-multiline'],
+//             'vue/no-mutating-props': 'error',
+//             'vue/no-unused-components': 'warn',
+//             'vue/multi-word-component-names': 'off',
+//         },
+//     },
+// )
